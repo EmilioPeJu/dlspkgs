d72afb9fc79b3755fc244a73ceb40685c7024828
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-08-13 17:23:35 +0100

    Use system ffmpeg for ffmpegServer

--- dls-epics-ffmpegserver/default.nix
+++ dls-epics-ffmpegserver/default.nix
@@ -1,17 +1,17 @@
-{ epicsRepoBaseUrl, readline, libxml2, yasm, buildEpicsModule, dls-epics-asyn
-, dls-epics-adcore, dls-epics-adsimdetector }:
+{ epicsRepoBaseUrl, readline, libxml2, ffmpeg-full, buildEpicsModule
+, dls-epics-asyn, dls-epics-adcore, dls-epics-adsimdetector }:
 
 buildEpicsModule {
   name = "dls-epics-ffmpegserver";
   buildInputs = [
     readline
     libxml2.dev
-    yasm
+    ffmpeg-full
     dls-epics-asyn
     dls-epics-adcore
     dls-epics-adsimdetector
   ];
-  patches = [ ./dont-build-yasm.patch ];
+  patches = [ ./use-sys-libs.patch ];
   src = builtins.fetchGit {
     url = "${epicsRepoBaseUrl}/ffmpegServer";
     ref = "dls-master";
