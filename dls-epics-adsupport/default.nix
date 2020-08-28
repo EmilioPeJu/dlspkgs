316e19e48c6cc8d51ef3d28bfa0124415b90dc8b
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-08-28 01:56:30 +0100

    adsupport: just should provide CBF

--- dls-epics-adsupport/default.nix
+++ dls-epics-adsupport/default.nix
@@ -9,21 +9,15 @@ buildEpicsModule {
   preConfigure = ''
     cat << EOF >  configure/CONFIG_SITE.linux-x86_64.Common
     CROSS_COMPILER_TARGET_ARCHS =
-    WITH_HDF5     = YES
-    HDF5_EXTERNAL = NO
-    WITH_SZIP     = YES
-    SZIP_EXTERNAL = NO
-    WITH_JPEG     = YES
-    JPEG_EXTERNAL = NO
-    WITH_TIFF     = YES
-    TIFF_EXTERNAL = NO
-    WITH_XML2 = YES
-    XML2_EXTERNO = NO
-    WITH_ZLIB     = YES
+    WITH_HDF5     = NO
+    WITH_SZIP     = NO
+    WITH_JPEG     = NO
+    WITH_TIFF     = NO
+    WITH_XML2     = NO
+    WITH_ZLIB     = NO
     ZLIB_EXTERNAL = NO
-    WITH_BLOSC    = YES
-    BLOSC_EXTERNAL = NO
-    WITH_CBF      = NO
+    WITH_BLOSC    = NO
+    WITH_CBF      = YES
     CBF_EXTERNAL  = NO
     EOF
   '';
