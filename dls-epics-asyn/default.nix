9ff616deb2215208be7bdbff67ab1e1f7ef901c9
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-02-18 01:00:23 +0000

    Fix asyn build

--- dls-epics-asyn/default.nix
+++ dls-epics-asyn/default.nix
@@ -1,10 +1,16 @@
-{ epicsRepoBaseUrl, buildEpicsModule, doxygen }:
+{ epicsRepoBaseUrl, buildEpicsModule, doxygen, libtirpc, rpcsvc-proto }:
 
 buildEpicsModule {
   name = "dls-epics-asyn";
-  buildInputs = [ doxygen ];
+  buildInputs = [ doxygen libtirpc libtirpc.dev rpcsvc-proto ];
   src = builtins.fetchGit {
     url = "${epicsRepoBaseUrl}/asyn";
     ref = "dls-master";
   };
+  postConfigure = ''
+    cat >> configure/CONFIG_SITE <<EOF
+    TIRPC=YES
+    USR_INCLUDES_Linux += -I${libtirpc.dev}/include/tirpc
+    EOF
+  '';
 }
