1eecd220d1e13fccec56d8bd9e2bf5480bf3cc73
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-02-23 12:43:23 +0000

    Remove uneeded patches
    The repository included them

--- dls-epics-streamdevice/default.nix
+++ dls-epics-streamdevice/default.nix
@@ -3,7 +3,6 @@
 buildEpicsModule {
   name = "dls-epics-streamdevice";
   buildInputs = [ readline dls-epics-asyn ];
-  patches = [ ./fix-build.patch ./fix-builder.patch ];
   src = builtins.fetchGit {
     url = "${epicsRepoBaseUrl}/streamdevice";
     ref = "dls-master";
