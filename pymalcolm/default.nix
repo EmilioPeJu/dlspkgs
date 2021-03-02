f647ba6c7c535d9930b936ae342174e0a00a3313
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-03-02 00:48:28 +0000

    Pymalcolm doesn't need my patches anymore

--- pymalcolm/default.nix
+++ pymalcolm/default.nix
@@ -1,38 +1,25 @@
 { epicsRepoBaseUrl, buildPythonPackage, enum-compat, tornado, numpy, ruamel_yaml
 , h5py, pygelf, plop, p4p, annotypes, cothread, scanpointgenerator, vdsgen, nose
-, mock, pytest-timeout }:
+, mock, pytest-timeout, ipython }:
 buildPythonPackage rec {
   pname = "pymalcolm";
   version = "4.2";
   doCheck = false;
   src = builtins.fetchGit { url = "${epicsRepoBaseUrl}/pymalcolm"; };
-  postConfigure = ''
-    # relax version constraints a bit
-    sed -i 's/==/>=/g' setup.py
-    # you get enum34 from enum-compat
-    sed -i '/enum34/d' setup.py
-    # typing is free in python 3.7
-    sed -i '/typing/d' setup.py
-    sed -i 's/tornado>=5.1.1/tornado>=5.1/g' setup.py
-  '';
-  patches = [
-    ./fix-cmd-string.patch
-    ./include-web-content.patch
-    ./disable-site-log-handlers.patch
-  ];
   buildInputs = [ nose mock pytest-timeout ];
   propagatedBuildInputs = [
-    enum-compat
-    tornado
-    numpy
-    ruamel_yaml
-    h5py
-    pygelf
-    plop
-    p4p
     annotypes
     cothread
+    enum-compat
+    h5py
+    ipython
+    numpy
+    p4p
+    plop
+    pygelf
+    ruamel_yaml
     scanpointgenerator
+    tornado
     vdsgen
   ];
 }
