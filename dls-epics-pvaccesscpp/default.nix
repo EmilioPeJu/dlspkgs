c39fd03dcbf0c2b74b7a4b27a3d1e90bf5ed2ead
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-03-02 01:57:05 +0000

    Link pvacess binaries in bin folder

--- dls-epics-pvaccesscpp/default.nix
+++ dls-epics-pvaccesscpp/default.nix
@@ -8,4 +8,10 @@ buildEpicsModule rec {
     url = "${epicsRepoBaseUrl}/pvaccesscpp";
     ref = "dls-master";
   };
+  postInstall = ''
+    cd $out/bin
+    for i in linux-x86_64/*; do
+        ln -s "$i"
+    done
+  '';
 }
