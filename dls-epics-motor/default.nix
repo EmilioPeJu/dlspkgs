2e21d061d463a0392c5ce73bd58cbffc85be51ce
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-07-01 00:56:02 +0100

    Get motor from upstream and add missing files

--- dls-epics-motor/default.nix
+++ dls-epics-motor/default.nix
@@ -1,11 +1,17 @@
-{ epicsRepoBaseUrl, buildEpicsModule, dls-epics-busy, dls-epics-asyn }:
+{ epicsRepoBaseUrl, fetchgit, buildEpicsModule, dls-epics-busy, dls-epics-asyn
+}:
 
 buildEpicsModule {
   name = "dls-epics-motor";
   buildInputs = [ dls-epics-busy dls-epics-asyn ];
-  src = builtins.fetchGit {
-    url = "${epicsRepoBaseUrl}/motor";
-    ref = "dls-master";
+  extraEtc = ./etc;
+  extraDb = ./Db;
+  extraEdl = ./edl;
+  src = fetchgit {
+    url = "https://github.com/epics-modules/motor";
+    rev = "R7-2-2";
+    sha256 = "07lv2hw1hij5i00qlcnpy8cqypic5pyj4yn782kaddpkf28371jx";
+    fetchSubmodules = true;
   };
-  patches = [ ./no-cross-compile.patch ./builder-to-py3.patch ];
+  patches = [ ./build-edl-folder.patch ];
 }
