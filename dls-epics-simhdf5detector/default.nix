0814d5aa30ed33b4d11e3a4c402fe5f2e06888a4
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2021-07-01 01:01:22 +0100

    Fix missing include in simhdf5detector

--- dls-epics-simhdf5detector/default.nix
+++ dls-epics-simhdf5detector/default.nix
@@ -4,5 +4,5 @@ buildEpicsModule {
   name = "dls-epics-simHDF5Detector";
   buildInputs = [ dls-epics-asyn dls-epics-adcore hdf5 ];
   src = builtins.fetchGit { url = "${epicsRepoBaseUrl}/simhdf5detector"; };
-  patches = [ ./builder-to-py3.patch ];
+  patches = [ ./builder-to-py3.patch ./fix-missing-include.patch ];
 }
