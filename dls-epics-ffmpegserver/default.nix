b83471faf660a33dca6137cb24dbea5572b1c11c
Emilio Perez <emiliopeju@gmail.com>
2022-11-15 16:57:44 +0000

    Temporary solution to multiple symbol definitions in ffmpegServer

--- dls-epics-ffmpegserver/default.nix
+++ dls-epics-ffmpegserver/default.nix
@@ -7,6 +7,7 @@ buildEpicsModule {
     [ readline libxml2.dev ffmpeg-full dls-epics-asyn dls-epics-adcore ];
   propagatedBuildInputs = [ ffmpeg-full ];
   extraEtc = ./etc;
+  NIX_CFLAGS_COMPILE = "-fcommon";
   src = fetchgit {
     url = "https://github.com/areaDetector/ffmpegServer";
     rev = "9fe0d46b49572c229b20d5cd6054fd6fb3cb419f";
