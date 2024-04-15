25beec24662842f5eecf4e2b941e6b73cd7816a7
Emilio Perez <emiliopeju@gmail.com>
2024-04-15 13:03:16 +0100

    Update cothread version
    A little hack was needed to hardcode EPICS_BASE path in the code

--- cothread/default.nix
+++ cothread/default.nix
@@ -1,11 +1,17 @@
-{ epicsRepoBaseUrl, buildPythonPackage, dls-epics-base, numpy }:
+{ epicsRepoBaseUrl, buildPythonPackage, dls-epics-base, numpy, setuptools,
+  setuptools_scm }:
 buildPythonPackage rec {
   pname = "cothread";
-  version = "2.16";
+  version = "2.20.0";
+  pyproject = true;
   doCheck = false;
-  propagatedBuildInputs = [ dls-epics-base numpy ];
+  propagatedBuildInputs = [ dls-epics-base numpy setuptools setuptools_scm ];
   src = builtins.fetchGit {
-    url = "${epicsRepoBaseUrl}/cothread";
-    ref = "refs/tags/2-16";
+    url = "https://github.com/DiamondLightSource/cothread.git";
+    ref = "refs/tags/${version}";
   };
+  postPatch = ''
+    sed -i 's#libca_path = .*#libca_path = "${dls-epics-base}/lib/linux-x86_64"#' \
+      src/cothread/load_ca.py
+  '';
 }
